5f6b72bda6d6be06db0359b1cf984eaaf929b41f
dovp-1m <petrosius133@gmail.com>
2026-04-22 11:25:50 +0300

    Delete

--- application/views/home/404.php
+++
@@ -1,8 +0,0 @@
-<div class="text-center py-5">
-    <div class="display-1 fw-bold text-primary mb-3">404</div>
-    <h2 class="mb-3">Page Not Found</h2>
-    <p class="text-muted mb-4">The page you are looking for does not exist or has been removed.</p>
-    <a href="<?= site_url('blog') ?>" class="btn btn-primary">
-        <i class="bi bi-house me-2"></i>Go Home
-    </a>
-</div>
